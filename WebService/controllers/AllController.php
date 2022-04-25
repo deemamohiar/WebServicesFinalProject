b8961740394fe12f469501dbeb2b2756908097f2
LisaHanna13 <losahanna2001@gmail.com>
2022-04-24 21:55:58 -0400

    AllController is fully done

--- WebService/controllers/AllController.php
+++ WebService/controllers/AllController.php
@@ -22,7 +22,8 @@ class AllController {
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         
         $response = curl_exec($curl);
-
+        echo "<a style= 'text-decoration: none;' 'color: black;' 'text-shadow: 0px 0px 3px #a8d8ff;' 'font-family: Verdana;' 'font-size: 18px;' href='/WebServicesFinalProject/Client/ClientController/index'>Back to Home Page</a>";
+        echo "<br><br>";
         $counter = 0;
         for ($i = 0; $i < strlen($response); $i++) {
             if ($response[$i] == "{") {
