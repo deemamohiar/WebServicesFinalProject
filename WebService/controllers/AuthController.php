486d4a7387ce92961a0713a8fae325b4dbab4121
Deema Mohiar <deema2002@live.ca>
2022-04-25 14:10:21 -0400

    removed debug method

--- WebService/controllers/AuthController.php
+++ WebService/controllers/AuthController.php
@@ -32,13 +32,5 @@
             header("WWW-Authenticate: Bearer $token");
             return $token;
         }
-
-        public function debug_to_console($data) {
-            $output = $data;
-            if (is_array($output))
-                $output = implode(',', $output);
-        
-            echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-        }
     }
 ?>
